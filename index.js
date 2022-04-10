36c4a15c2c4636db4a421c0d97311b7b066618e0
unknown <vladancupric@gmail.com>
2022-04-10 10:41:48 +0200

    New commits

--- index.js
+++ index.js
@@ -1,69 +1,427 @@
-
 const main = () => {
+  let result1;
+  let result2;
 
-
-let result1;
-let result2;
-
-  const generateResultGoals = function() {
-    for (var i = 0; i< worldTeams.length;i++){
-      return Math.floor((Math.random() * (8)));
+  const generateResultGoals = function () {
+    for (var i = 0; i < worldTeams.length; i++) {
+      return Math.floor(Math.random() * 8);
     }
+  };
+
+  // Group
+  const groupA = [
+    {
+      name: "Katar",
+      rank: 51,
+      points: 0,
+      goalsFor: 0,
+      goalsAgainstt: 0,
+      goalsDifference: 0,
+      win: 0,
+      draw: 0,
+      lost: 0,
+    },
+    {
+      name: "Ekvador",
+      rank: 46,
+      points: 0,
+      goalsFor: 0,
+      goalsAgainst: 0,
+      goalsDifference: 0,
+      win: 0,
+      draw: 0,
+      lost: 0,
+    },
+    {
+      name: "Senegal",
+      rank: 20,
+      points: 0,
+      goalsFor: 0,
+      goalsAgainst: 0,
+      goalsDifference: 0,
+      win: 0,
+      draw: 0,
+      lost: 0,
+    },
+    {
+      name: "Holandija",
+      rank: 10,
+      points: 0,
+      goalsFor: 0,
+      goalsAgainst: 0,
+      goalsDifference: 0,
+      win: 0,
+      draw: 0,
+      lost: 0,
+    },
+  ];
+  const groupB = [
+    {
+      name: "Engleska",
+      rank: 5,
+      points: 0,
+      goalsFor: 0,
+      goalsAgainst: 0,
+      goalsDifference: 0,
+      win: 0,
+      draw: 0,
+      lost: 0,
+    },
+    {
+      name: "Iran",
+      rank: 21,
+      points: 0,
+      goalsFor: 0,
+      goalsAgainst: 0,
+      goalsDifference: 0,
+      win: 0,
+      draw: 0,
+      lost: 0,
+    },
+    {
+      name: "SAD",
+      rank: 15,
+      points: 0,
+      goalsFor: 0,
+      goalsAgainst: 0,
+      goalsDifference: 0,
+      win: 0,
+      draw: 0,
+      lost: 0,
+    },
+    {
+      name: "Ukrajina",
+      rank: 27,
+      points: 0,
+      goalsFor: 0,
+      goalsAgainst: 0,
+      goalsDifference: 0,
+      win: 0,
+      draw: 0,
+      lost: 0,
+    },
+  ];
+  const groupC = [
+    {
+      name: "Argentina",
+      rank: 4,
+      points: 0,
+      goalsFor: 0,
+      goalsAgainst: 0,
+      goalsDifference: 0,
+      win: 0,
+      draw: 0,
+      lost: 0,
+    },
+    {
+      name: "Saudijska Arabija",
+      rank: 49,
+      points: 0,
+      goalsFor: 0,
+      goalsAgainst: 0,
+      goalsDifference: 0,
+      win: 0,
+      draw: 0,
+      lost: 0,
+    },
+    {
+      name: "Meksiko",
+      rank: 9,
+      points: 0,
+      goalsFor: 0,
+      goalsAgainst: 0,
+      goalsDifference: 0,
+      win: 0,
+      draw: 0,
+      lost: 0,
+    },
+    {
+      name: "Poljska",
+      rank: 26,
+      points: 0,
+      goalsFor: 0,
+      goalsAgainst: 0,
+      goalsDifference: 0,
+      win: 0,
+      draw: 0,
+      lost: 0,
+    },
+  ];
+  const groupD = [
+    {
+      name: "Francuska",
+      rank: 3,
+      points: 0,
+      goalsFor: 0,
+      goalsAgainst: 0,
+      goalsDifference: 0,
+      win: 0,
+      draw: 0,
+      lost: 0,
+    },
+    {
+      name: "Peru",
+      rank: 22,
+      points: 0,
+      goalsFor: 0,
+      goalsAgainst: 0,
+      goalsDifference: 0,
+      win: 0,
+      draw: 0,
+      lost: 0,
+    },
+    {
+      name: "Danska",
+      rank: 11,
+      points: 0,
+      goalsFor: 0,
+      goalsAgainst: 0,
+      goalsDifference: 0,
+      win: 0,
+      draw: 0,
+      lost: 0,
+    },
+    {
+      name: "Tunis",
+      rank: 35,
+      points: 0,
+      goalsFor: 0,
+      goalsAgainst: 0,
+      goalsDifference: 0,
+      win: 0,
+      draw: 0,
+      lost: 0,
+    },
+  ];
+  const groupE = [
+    {
+      name: "Spanija",
+      rank: 7,
+      points: 0,
+      goalsFor: 0,
+      goalsAgainst: 0,
+      goalsDifference: 0,
+      win: 0,
+      draw: 0,
+      lost: 0,
+    },
+    {
+      name: "Novi Zeland",
+      rank: 101,
+      points: 0,
+      goalsFor: 0,
+      goalsAgainst: 0,
+      goalsDifference: 0,
+      win: 0,
+      draw: 0,
+      lost: 0,
+    },
+    {
+      name: "Nemacka",
+      rank: 12,
+      points: 0,
+      goalsFor: 0,
+      goalsAgainst: 0,
+      goalsDifference: 0,
+      win: 0,
+      draw: 0,
+      lost: 0,
+    },
+    {
+      name: "Japan",
+      rank: 23,
+      points: 0,
+      goalsFor: 0,
+      goalsAgainst: 0,
+      goalsDifference: 0,
+      win: 0,
+      draw: 0,
+      lost: 0,
+    },
+  ];
+  const groupF = [
+    {
+      name: "Belgija",
+      rank: 2,
+      points: 0,
+      goalsFor: 0,
+      goalsAgainst: 0,
+      goalsDifference: 0,
+      win: 0,
+      draw: 0,
+      lost: 0,
+    },
+    {
+      name: "Kanada",
+      rank: 38,
+      points: 0,
+      goalsFor: 0,
+      goalsAgainst: 0,
+      goalsDifference: 0,
+      win: 0,
+      draw: 0,
+      lost: 0,
+    },
+    {
+      name: "Maroko",
+      rank: 24,
+      points: 0,
+      goalsFor: 0,
+      goalsAgainst: 0,
+      goalsDifference: 0,
+      win: 0,
+      draw: 0,
+      lost: 0,
+    },
+    {
+      name: "Hrvatska",
+      rank: 16,
+      points: 0,
+      goalsFor: 0,
+      goalsAgainst: 0,
+      goalsDifference: 0,
+      win: 0,
+      draw: 0,
+      lost: 0,
+    },
+  ];
+  const groupG = [
+    {
+      name: "Brazil",
+      rank: 1,
+      points: 0,
+      goalsFor: 0,
+      goalsAgainst: 0,
+      goalsDifference: 0,
+      win: 0,
+      draw: 0,
+      lost: 0,
+    },
+    {
+      name: "Svajcarska",
+      rank: 14,
+      points: 0,
+      goalsFor: 0,
+      goalsAgainst: 0,
+      goalsDifference: 0,
+      win: 0,
+      draw: 0,
+      lost: 0,
+    },
+    {
+      name: "Kamerun",
+      rank: 37,
+      points: 0,
+      goalsFor: 0,
+      goalsAgainst: 0,
+      goalsDifference: 0,
+      win: 0,
+      draw: 0,
+      lost: 0,
+    },
+    {
+      name: "Srbija",
+      rank: 25,
+      points: 0,
+      goalsFor: 0,
+      goalsAgainst: 0,
+      goalsDifference: 0,
+      win: 0,
+      draw: 0,
+      lost: 0,
+    },
+  ];
+  const groupH = [
+    {
+      name: "Portugal",
+      rank: 8,
+      points: 0,
+      goalsFor: 0,
+      goalsAgainst: 0,
+      goalsDifference: 0,
+      win: 0,
+      draw: 0,
+      lost: 0,
+    },
+    {
+      name: "Gana",
+      rank: 60,
+      points: 0,
+      goalsFor: 0,
+      goalsAgainst: 0,
+      goalsDifference: 0,
+      win: 0,
+      draw: 0,
+      lost: 0,
+    },
+    {
+      name: "Urugvaj",
+      rank: 13,
+      points: 0,
+      goalsFor: 0,
+      goalsAgainst: 0,
+      goalsDifference: 0,
+      win: 0,
+      draw: 0,
+      lost: 0,
+    },
+    {
+      name: "Juzna koreja",
+      rank: 29,
+      points: 0,
+      goalsFor: 0,
+      goalsAgainst: 0,
+      goalsDifference: 0,
+      win: 0,
+      draw: 0,
+      lost: 0,
+    },
+  ];
+
+  let groups = [];
+groups.push(groupA,groupB,groupC,groupD,groupE,groupF,groupG,groupH);
+
+  let groupTable = function(group) {
+    for (let country in group){
+      let  {
+        name: name,
+        teamRank: rank,
+        points:points,
+        goalsFor: goalsFor,
+        goalsAgainst:goalsAgainst ,
+        goalsDifference: goalsDifference,
+        win:win,
+        draw: draw,
+        lost:lost
+      }  = country;
+      console.table(
+        `${name} (${rank})  ${win} ${draw} ${lost} ${goalsFor}:${goalsAgainst} ${goalsDifference}${points}`
+
+      )
+
+  
     }
+    console.log(groupTable(groupA))
+  
 
-    // Group
-    const groupA = [
-      {name:"Katar", rank:51, points:0, goalsFor:0, goalsAgainstt:0,goalsDifference:0, win:0, draw:0, lost:0},
-      {name:"Ekvador", rank:46, points:0, goalsFor:0, goalsAgainst:0,goalsDifference:0, win:0, draw:0, lost:0},
-      {name:"Senegal", rank:20, points:0, goalsFor:0, goalsAgainst:0,goalsDifference:0, win:0, draw:0, lost:0},
-      {name:"Holandija", rank:10, points:0, goalsFor:0, goalsAgainst:0,goalsDifference:0, win:0, draw:0, lost:0},
-
-    ]
-    const groupB = [
-      {name:"Engleska", rank:5, points:0, goalsFor:0, goalsAgainst:0,goalsDifference:0, win:0, draw:0, lost:0},
-      {name:"Iran", rank:21, points:0, goalsFor:0, goalsAgainst:0,goalsDifference:0, win:0, draw:0, lost:0},
-      {name:"SAD", rank:15, points:0, goalsFor:0, goalsAgainst:0,goalsDifference:0, win:0, draw:0, lost:0},
-      {name:"Ukrajina", rank:27, points:0, goalsFor:0, goalsAgainst:0,goalsDifference:0, win:0, draw:0, lost:0},
-
-    ]
-    const groupC = [
-      {name:"Argentina", rank:4, points:0, goalsFor:0, goalsAgainst:0,goalsDifference:0, win:0, draw:0, lost:0},
-      {name:"Saudijska Arabija", rank:49, points:0, goalsFor:0, goalsAgainst:0,goalsDifference:0, win:0, draw:0, lost:0},
-      {name:"Meksiko", rank:9, points:0, goalsFor:0, goalsAgainst:0,goalsDifference:0, win:0, draw:0, lost:0},
-      {name:"Poljska", rank:26, points:0, goalsFor:0, goalsAgainst:0,goalsDifference:0, win:0, draw:0, lost:0},
-
-    ]
-    const groupD = [
-      {name:"Francuska", rank:3, points:0, goalsFor:0, goalsAgainst:0,goalsDifference:0, win:0, draw:0, lost:0},
-      {name:"Peru", rank:22, points:0, goalsFor:0, goalsAgainst:0,goalsDifference:0, win:0, draw:0, lost:0},
-      {name:"Danska", rank:11, points:0, goalsFor:0, goalsAgainst:0,goalsDifference:0, win:0, draw:0, lost:0},
-      {name:"Tunis", rank:35, points:0, goalsFor:0, goalsAgainst:0,goalsDifference:0, win:0, draw:0, lost:0},
-    ]
-    const groupE = [
-      {name:"Spanija", rank:7, points:0, goalsFor:0, goalsAgainst:0,goalsDifference:0, win:0, draw:0, lost:0},
-      {name:"Novi Zeland", rank:101, points:0, goalsFor:0, goalsAgainst:0,goalsDifference:0, win:0, draw:0, lost:0},
-      {name:"Nemacka", rank:12, points:0, goalsFor:0, goalsAgainst:0,goalsDifference:0, win:0, draw:0, lost:0},
-      {name:"Japan", rank:23, points:0, goalsFor:0, goalsAgainst:0,goalsDifference:0, win:0, draw:0, lost:0},
-    ]
-    const groupF = [
-      {name:"Belgija", rank:2, points:0, goalsFor:0, goalsAgainst:0,goalsDifference:0, win:0, draw:0, lost:0},
-      {name:"Kanada", rank:38, points:0, goalsFor:0, goalsAgainst:0,goalsDifference:0, win:0, draw:0, lost:0},
-      {name:"Maroko", rank:24, points:0, goalsFor:0, goalsAgainst:0,goalsDifference:0, win:0, draw:0, lost:0},
-      {name:"Hrvatska", rank:16, points:0, goalsFor:0, goalsAgainst:0,goalsDifference:0, win:0, draw:0, lost:0},
-    ]
-    const groupG = [
-      {name:"Brazil", rank:1, points:0, goalsFor:0, goalsAgainst:0,goalsDifference:0, win:0, draw:0, lost:0},
-      {name:"Svajcarska", rank:14, points:0, goalsFor:0, goalsAgainst:0,goalsDifference:0, win:0, draw:0, lost:0},
-      {name:"Kamerun", rank:37, points:0, goalsFor:0, goalsAgainst:0,goalsDifference:0, win:0, draw:0, lost:0},
-      {name:"Srbija", rank:25, points:0, goalsFor:0, goalsAgainst:0,goalsDifference:0, win:0, draw:0, lost:0},
-    ]
-    const groupH = [
-      {name:"Portugal", rank:8, points:0, goalsFor:0, goalsAgainst:0,goalsDifference:0, win:0, draw:0, lost:0},
-      {name:"Gana", rank:60, points:0, goalsFor:0, goalsAgainst:0,goalsDifference:0, win:0, draw:0, lost:0},
-      {name:"Urugvaj", rank:13, points:0, goalsFor:0, goalsAgainst:0,goalsDifference:0, win:0, draw:0, lost:0},
-      {name:"Juzna koreja", rank:29, points:0, goalsFor:0, goalsAgainst:0,goalsDifference:0, win:0, draw:0, lost:0},
-    ]
-
+  }
 };
 
 main();
+
+// var teams = ["a", "b", "c", "d"]
+
+// function possibleMatches(teams) {
+//   for (var firstTeam = 0; firstTeam < teams.length; firstTeam++) {
+//     for (var secondTeam = firstTeam + 1; secondTeam < teams.length; secondTeam++) {
+//       console.log( teams[firstTeam],":", teams[secondTeam])
+//     }
+//   }
+
+// }
+  
+// possibleMatches(teams)
+
+
+
+
