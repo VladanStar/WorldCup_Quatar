db814bb71962dad7bf149ae8bc1f799672bb341a
unknown <vladancupric@gmail.com>
2022-04-10 09:03:26 +0200

    New commits

--- index.js
+++ index.js
@@ -13,8 +13,57 @@ let result2;
 
     // Group
     const groupA = [
-      {name:"Katar", rank:1, points:0, goalsFor:0, golasagains:0,goalsDifference:0, win:0, draw:0, lost:0, }
+      {name:"Katar", rank:51, points:0, goalsFor:0, golasagains:0,goalsDifference:0, win:0, draw:0, lost:0},
+      {name:"Ekvador", rank:46, points:0, goalsFor:0, golasagains:0,goalsDifference:0, win:0, draw:0, lost:0},
+      {name:"Senegal", rank:20, points:0, goalsFor:0, golasagains:0,goalsDifference:0, win:0, draw:0, lost:0},
+      {name:"Holandija", rank:10, points:0, goalsFor:0, golasagains:0,goalsDifference:0, win:0, draw:0, lost:0},
+
     ]
+    const groupB = [
+      {name:"Engleska", rank:5, points:0, goalsFor:0, golasagains:0,goalsDifference:0, win:0, draw:0, lost:0},
+      {name:"Iran", rank:21, points:0, goalsFor:0, golasagains:0,goalsDifference:0, win:0, draw:0, lost:0},
+      {name:"SAD", rank:15, points:0, goalsFor:0, golasagains:0,goalsDifference:0, win:0, draw:0, lost:0},
+      {name:"Ukrajina", rank:27, points:0, goalsFor:0, golasagains:0,goalsDifference:0, win:0, draw:0, lost:0},
+
+    ]
+    const groupC = [
+      {name:"Argentina", rank:4, points:0, goalsFor:0, golasagains:0,goalsDifference:0, win:0, draw:0, lost:0},
+      {name:"Saudijska Arabija", rank:49, points:0, goalsFor:0, golasagains:0,goalsDifference:0, win:0, draw:0, lost:0},
+      {name:"Meksiko", rank:9, points:0, goalsFor:0, golasagains:0,goalsDifference:0, win:0, draw:0, lost:0},
+      {name:"Poljska", rank:26, points:0, goalsFor:0, golasagains:0,goalsDifference:0, win:0, draw:0, lost:0},
+
+    ]
+    const groupD = [
+      {name:"Francuska", rank:3, points:0, goalsFor:0, golasagains:0,goalsDifference:0, win:0, draw:0, lost:0},
+      {name:"Peru", rank:22, points:0, goalsFor:0, golasagains:0,goalsDifference:0, win:0, draw:0, lost:0},
+      {name:"Danska", rank:11, points:0, goalsFor:0, golasagains:0,goalsDifference:0, win:0, draw:0, lost:0},
+      {name:"Tunis", rank:35, points:0, goalsFor:0, golasagains:0,goalsDifference:0, win:0, draw:0, lost:0},
+    ]
+    const groupE = [
+      {name:"Spanija", rank:7, points:0, goalsFor:0, golasagains:0,goalsDifference:0, win:0, draw:0, lost:0},
+      {name:"Novi Zeland", rank:101, points:0, goalsFor:0, golasagains:0,goalsDifference:0, win:0, draw:0, lost:0},
+      {name:"Nemacka", rank:12, points:0, goalsFor:0, golasagains:0,goalsDifference:0, win:0, draw:0, lost:0},
+      {name:"Japan", rank:23, points:0, goalsFor:0, golasagains:0,goalsDifference:0, win:0, draw:0, lost:0},
+    ]
+    const groupF = [
+      {name:"Belgija", rank:2, points:0, goalsFor:0, golasagains:0,goalsDifference:0, win:0, draw:0, lost:0},
+      {name:"Kanada", rank:38, points:0, goalsFor:0, golasagains:0,goalsDifference:0, win:0, draw:0, lost:0},
+      {name:"Maroko", rank:24, points:0, goalsFor:0, golasagains:0,goalsDifference:0, win:0, draw:0, lost:0},
+      {name:"Hrvatska", rank:16, points:0, goalsFor:0, golasagains:0,goalsDifference:0, win:0, draw:0, lost:0},
+    ]
+    const groupG = [
+      {name:"Brazil", rank:1, points:0, goalsFor:0, golasagains:0,goalsDifference:0, win:0, draw:0, lost:0},
+      {name:"Svajcarska", rank:14, points:0, goalsFor:0, golasagains:0,goalsDifference:0, win:0, draw:0, lost:0},
+      {name:"Kamerun", rank:37, points:0, goalsFor:0, golasagains:0,goalsDifference:0, win:0, draw:0, lost:0},
+      {name:"Srbija", rank:25, points:0, goalsFor:0, golasagains:0,goalsDifference:0, win:0, draw:0, lost:0},
+    ]
+    const groupH = [
+      {name:"Portugal", rank:8, points:0, goalsFor:0, golasagains:0,goalsDifference:0, win:0, draw:0, lost:0},
+      {name:"Gana", rank:60, points:0, goalsFor:0, golasagains:0,goalsDifference:0, win:0, draw:0, lost:0},
+      {name:"Urugvaj", rank:13, points:0, goalsFor:0, golasagains:0,goalsDifference:0, win:0, draw:0, lost:0},
+      {name:"Juzna koreja", rank:29, points:0, goalsFor:0, golasagains:0,goalsDifference:0, win:0, draw:0, lost:0},
+    ]
+
 };
 
 main();
